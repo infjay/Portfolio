3eaca86dbec377fb62099da37011a91a37dd598b
infjay <jawid.nm@gmail.com>
2023-02-06 22:55:34 +0100

    minimal changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     const handleSubmit = (e)=> {
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact",{
+        let response = fetch("http://localhost:5000/contact",{
         method: "POST",
         headers:{
             "Content-Type": "application/json;charset=utf-8",
