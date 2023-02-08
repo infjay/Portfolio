2f251ff9a96f01ecf8139ac9310a59053a376145
Jawid <41361157+infjay@users.noreply.github.com>
2023-02-08 20:35:36 +0100

    Mailer configured

--- server.js
+++ server.js
@@ -1,51 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cors = require('cors');
-const nodemailer = require('nodemailer');
- 
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
-// Server used to send email
+// server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/",router);
-app.listen(5000,()=> console.log("Server Up & Running"));
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+
 
 const contactEmail = nodemailer.createTransport({
-    service:"gmail",
-    auth: {
-        user:process.env.username_gmail,
-        pass:process.env.password
-    },
-})
+  service: 'gmail',
+  auth: {
+    user: process.env.USERNAME_GMAIL,
+    pass: process.env.PASSWORD
+  },
+});
+console.log(contactEmail);
 
-contactEmail.verify((error)=> {
-    if(error){
-        console.log(error);
-    } else {
-        console.log("Ready to send email");
-    }
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
 });
 
-router.post("/contact",(req,res)=> {
-    const name = req.body.firstName + req.body.lastName;
-    const email = req.body.email;
-    const message = req.body.message;
-    const phone = req.body.phone;
-    const mail = {
-        form: name,
-        to:process.env.username_gmail,
-        subject: "Contact Form Submission - MY Portfolio",
-        html:`<p>Name: ${name}</p>
-              <p>Email: ${email}</p>
-              <p>Phone: ${phone}</p>
-              <p>Message: ${message}</p>`
-    };
-    contactEmail.sendMail(mail,(error)=> {
-        if(error) {
-            res.json(error);
-        }else {
-            res.json({code:200, status:"Message Sent"})
-        }
-    });
-})
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: process.env.USERNAME_GMAIL,
+    to: process.env.USERNAME_GMAIL,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
